b6b0fb952462094f51ba700e6c37ace76f96ee53
Diego <114715116+Nyaommi@users.noreply.github.com>
2024-11-07 19:46:13 -0600

    Login completed
    The login is ready. By the moment we only have the admin main window.

---
+++ database.py
@@ -0,0 +1,51 @@
+import mysql.connector
+import bcrypt
+
+user = 'root'
+password = 'cinna123'
+database = 'controlescolar'
+host = 'localhost' #it's all in localhost cause it's a shcollar proyect... but we could do it in a web server.
+
+def hashPassword(password):
+  salt = bcrypt.gensalt()
+  hashed_password = bcrypt.hashpw(password.encode(), salt)
+  return hashed_password
+
+def verifyPassword(password, hashedPassword):
+  if isinstance(password, str):
+      password = password.encode() 
+  if isinstance(hashedPassword, str):
+      hashedPassword = hashedPassword.encode()
+  return bcrypt.checkpw(password, hashedPassword)
+
+def open():
+  conn = mysql.connector.connect(host = host, user = user, passwd = password, database = database)
+  return conn
+
+def close():
+  conn = open()
+  conn.close()
+
+def update():
+  hp = hashPassword('Cinna123')
+  conn = open()
+  cursor1 = conn.cursor()
+  sql = 'update administradores set password = %s where id = 1'
+  cursor1.execute(sql, (hp,))
+  conn.commit()
+  conn.close()
+
+def login(userType, mail, password):
+  conn = open()
+  cursor1 = conn.cursor()
+  sql = 'select * from '+userType+' where mail = %s;'
+  cursor1.execute(sql, (mail,))
+  row = cursor1.fetchone()
+  conn.close()
+  if row:
+    if verifyPassword(password, row[2]):
+      return True
+    else:
+      return False
+  else:
+    return('noUser')
---
+++ main.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+import database
+
+class App(tk.Tk):
+  def __init__(self):
+    super().__init__()
+    self.title('Login')
+    self.geometry('500x350')
+    self.currentUser = None
+
+    self.menuLogin()
+
+  def menuLogin(self):
+    for widget in self.winfo_children():
+      widget.destroy()
+    lbTittle = ttk.Label(self, text='Control Escolar')
+
+    self.grid_rowconfigure((0,1,2,3, 4), weight=1)
+    self.grid_columnconfigure((0,1,2), weight=1)
+
+    frameRadio = tk.Frame(self)
+    frameRadio.config(relief="groove")
+    frameRadio.config(bd=5)
+    frameRadio.grid(row=3, column=1)
+    userType = tk.StringVar()
+
+    radioAdmin = tk.Radiobutton(frameRadio, text="Coordinador", variable=userType, value='administradores')
+    radioAdmin.pack(side="left", padx=3)
+    radioStudent = tk.Radiobutton(frameRadio, text="Alumno", variable=userType, value='alumnos')
+    radioStudent.pack(side="left", padx=3)
+    radioTeacher = tk.Radiobutton(frameRadio, text="Maestro", variable=userType, value='maestros')
+    radioTeacher.pack(side="left", padx=3)
+
+    lbUser = ttk.Label(self, text='Email:')
+    lbPassword = ttk.Label(self, text='Password:')
+
+    entryUser = ttk.Entry(self)
+    entryPassword = ttk.Entry(self, show='*')
+
+    lbTittle.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
+
+    lbUser.grid(row=1, column=1, sticky=W)
+    lbPassword.grid(row=2, column=1, sticky=W)
+
+    entryUser.grid(row=1, column=1, padx=(60, 0), sticky=W+E)
+    entryPassword.grid(row=2, column=1, padx=(60,0), sticky=W+E)
+
+    buttonLogin = ttk.Button(self, text='Login', command=lambda: self.login(userType.get(), entryUser.get(), entryPassword.get()))
+    buttonLogin.grid(row=4, column=1, padx=5, pady=5)
+
+  def login(self, userType, mail, password):
+    if userType == '':
+      messagebox.showwarning('Error de login', 'Por favor selecciones que tipo de usuario es')
+    else:
+      response = database.login(userType, mail, password)
+      if response == 'noUser':
+        messagebox.showerror('Usuario inexistente','Parece que no existe el mail ingresado')
+      elif response == False:
+        messagebox.showerror('Password incorrecto', 'El password introducido es incorrecto')
+      elif response == True:
+        print(userType)
+        if userType == 'administradores':
+          self.menuAdmin()
+        elif userType == 'alumnos':
+          pass
+        elif userType == 'maestros':
+          pass
+  def menuAdmin(self):
+    for widget in self.winfo_children():
+      widget.destroy()
+    self.title('Control Escolar -Coordinador-')
+    nb = ttk.Notebook(self)
+    nb.pack(fill='both', expand='yes')
+
+    frameCoordinadores = ttk.Frame(nb)
+    frameAlumnos = ttk.Frame(nb)
+    frameMaestros = ttk.Frame(nb)
+    frameMaterias = ttk.Frame(nb)
+    frameGrupos = ttk.Frame(nb)
+    frameHorarios = ttk.Frame(nb)
+    frameSalones = ttk.Frame(nb)
+    frameCarreras = ttk.Frame(nb)
+    framePlaneacion = ttk.Frame(nb)
+
+    nb.add(frameCoordinadores, text='Coordinadores')
+    nb.add(frameAlumnos, text='Alumnos')
+    nb.add(frameMaestros, text='Maestros')
+    nb.add(frameMaterias, text='Materias')
+    nb.add(frameGrupos, text='Grupos')
+    nb.add(frameHorarios, text='Horario')
+    nb.add(frameSalones, text='Salon')
+    nb.add(frameCarreras, text='Carrera')
+    nb.add(framePlaneacion, text='Planeacion')
+
+    
+
+
+
+if __name__ == '__main__':
+  app = App()
+  app.mainloop()
